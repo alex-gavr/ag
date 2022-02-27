2b548cf32b0836d10126df8470e74edad930d607
Alexander Gavrilenko <sashadrug@mail.ru>
2022-02-27 18:16:37 +0300

    Pic size change

--- js/randomPics.js
+++ js/randomPics.js
@@ -1,71 +1,12 @@
 export function randomPicsFull(){
-  let imgName = [
-    "1.jpg",
-    "2.jpg",
-    "3.jpg",
-    "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
-    "9.jpg",
-    "10.jpg",
-    "11.jpg",
-    "12.jpg",
-    "13.jpg",
-    "14.jpg",
-    "15.jpg",
-    "16.jpg",
-    "17.jpg",
-    "18.jpg",
-    "19.jpg",
-    "20.jpg",
-    "21.jpg",
-    "22.jpg",
-    "23.jpg",
-    "24.jpg",
-    "25.jpg",
-    "26.jpg",
-    "27.jpg",
-    "28.jpg",
-    "29.jpg",
-    "30.jpg",
-    "31.jpg",
-    "32.jpg",
-    "33.jpg",
-    "34.jpg",
-    "35.jpg",
-    "36.jpg",
-    "37.jpg",
-    "38.jpg",
-    "39.jpg",
-    "40.jpg",
-    "41.jpg",
-    "42.jpg",
-    "43.jpg",
-    "44.jpg",
-    "45.jpg"
-  ]
-
-
 
   let playButton = document.getElementById('playButton');
   let pics = document.querySelectorAll('.randomPic');
-  // let currentUrl = window.location.href;
-
-
-
-
+  
   playButton.addEventListener("click", () => {
     pics.forEach(pic =>{
-      let randomPic = imgName[Math.floor(Math.random()*imgName.length)];
-      // if (currentUrl == "http:///127.0.0.1:5500/game.html"){
-      //   pic.src = `/pics/forGame/${randomPic}`;
-      //   console.log(pic.src);
-      // } else {
-        // let t = randomPic.replace('.jpg','')
-        // console.log(t);
-        pic.src= `https://alex-gavr.github.io/ag/pics/forGame/${randomPic}`;
+      let randomPic = Math.floor(Math.random() * 45) + 1;
+        pic.src= `https://alex-gavr.github.io/ag/pics/forGame/${randomPic}.jpg`;
         console.log(pic.src);
       // }
     });
--- js/youWon.js
+++ js/youWon.js
@@ -1,14 +1,14 @@
+
 export function youWonFull(){
 
   let winButton = document.getElementById('youWon');
   let pic1 = "";
   let pic2 = "";
-  // let pic3 = "";
-  // const pic3 = document.getElementById('thirdImage').src;
   let winText = document.getElementById('winText');
   let win = 0;
 
   function youWon(){
+    
     if(pic1 === pic2){
     win++;
     winText.innerHTML = `Ты выиграл ${win} раз`;
@@ -21,12 +21,11 @@ export function youWonFull(){
       ease: 'elastic.out(1,0.3)'
     });
   }
-  }
-
-  playButton.addEventListener("click", () => {
-    pic1 = document.getElementById('firstImage').src;
-    pic2 = document.getElementById('secondImage').src;
-    // pic3 = document.getElementById('thirdImage').src;
-    youWon();
-  });
+}
+playButton.addEventListener("click", () => {
+  pic1 = document.getElementById('firstImage').src;
+  pic2 = document.getElementById('secondImage').src;
+  youWon();
+});
+    
 }
